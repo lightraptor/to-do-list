05659882fc0fa2f54fe780848d079e71e9fedc3e
LIGHTRAPTOR\qle23 <qle2318@gmail.com>
2023-07-09 11:38:15 +0700

    update icon edit and delete

--- js/index.js
+++ js/index.js
@@ -18,27 +18,29 @@ function addTask() {
     checkbox.type = "checkbox";
     checkbox.addEventListener("change", toggleTaskCompletion);
 
-    const editButton = document.createElement("button");
-    editButton.innerHTML = "Edit";
-    editButton.classList.add("btn", "btn-primary");
+    const editButton = document.createElement("i");
+    //editButton.innerHTML = "Edit";
+    // <i class="fa-solid fa-pen-to-square"></i>
+    // <i class="fa-solid fa-check"></i>
+    editButton.classList.add("fa-solid", "fa-pen-to-square");
     editButton.addEventListener("click", function() {
         if (!editMode) {
             enableEditMode(li);
-            editButton.innerHTML = "Save";
-            editButton.classList.remove("btn-primary");
-            editButton.classList.add("btn-success");
+            //editButton.innerHTML = "Save";
+            editButton.classList.remove("fa-pen-to-squarey");
+            editButton.classList.add("fa-check");
         } else {
             disableEditMode(li);
-            editButton.innerHTML = "Edit";
-            editButton.classList.remove("btn-success");
-            editButton.classList.add("btn-primary");
+            //editButton.innerHTML = "Edit";
+            editButton.classList.remove("fa-check");
+            editButton.classList.add("fa-pen-to-square");
         }
         editMode = !editMode;
     });
-
-    const deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.classList.add("btn", "btn-primary");
+    //<i class="fa-regular fa-trash-can"></i>
+    const deleteButton = document.createElement("i");
+    //deleteButton.innerHTML = "Delete";
+    deleteButton.classList.add("fa-regular", "fa-trash-can");
     deleteButton.addEventListener("click", function() {
         task.remove();
     });
@@ -54,7 +56,11 @@ function addTask() {
 
 function toggleTaskCompletion() {
     const li = this.nextElementSibling;
-    li.style.textDecoration = this.checked ? "line-through" : "none";
+    if (editMode == false) {
+        li.style.textDecoration = this.checked ? "line-through" : "none";
+    }
+    else this.checked = false;
+    
 }
 
 function enableEditMode(li) {
